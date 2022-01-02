13384ad4e925a4b3214d834e5b405ea36b72bb0d
Scripted <68810671+ScriptedDeveloper@users.noreply.github.com>
2022-01-02 15:29:24 +0100

    Update idt.c

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -13,7 +13,7 @@ void exception_handler(){
 void idt_set_desc(uint8_t vector, void* isr, uint8_t flags){
     IDTDesc* descriptor = &idt_desc[vector];
     descriptor->isr_low        = (uint32_t)isr & 0xFFFF;
-    descriptor->kernel_cs      = 0x08; // this value can be whatever offset your kernel code selector is in your GDT
+    descriptor->kernel_cs      = 0x08;
     descriptor->attributes     = flags;
     descriptor->isr_high       = (uint32_t)isr >> 16;
     descriptor->reserved       = 0;
