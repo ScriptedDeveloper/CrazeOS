e0ac68b0c6fef160f224bda826d80b50f75be805
Scripted <68810671+ScriptedDeveloper@users.noreply.github.com>
2022-01-10 21:29:06 +0100

    Update kernel.h

--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,15 +1,11 @@
 #pragma once
 #ifndef KERNEL_H_INCLUDED
-#define NULL 0
 #define KERNEL_H_INCLUDED
 #include <stdint.h>
 #include "keyboard/keyboard.h"
 #include "video/video.h"    
 
 static uint8_t stack[8192];
-static struct stivale2_header_tag_terminal;
-static struct stivale2_header_tag_framebuffer;
-static struct stivale2_header;
 int kmain();
 int kcharlen(char *c);
 long khash(char* string);
