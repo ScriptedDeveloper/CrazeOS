730c574c405f4a86295b679889a919827482c4fc
Scripted <68810671+ScriptedDeveloper@users.noreply.github.com>
2022-03-08 21:54:14 +0100

    Fixing push mistakes

--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,16 +1,15 @@
 #pragma once
-#ifndef KERNEL_H_INCLUDED
-#define KERNEL_H_INCLUDED
 #include <stdint.h>
 #include <stddef.h>
 #include "keyboard/keyboard.h"
 #include "video/video.h"
 #include "shell/shell.h"
 #include "string/string.h"
-
+#include "gdt/gdt.h"
+#include "idt/idt.h"
+#include "idt/isr.h"
 
 static uint8_t stack[8192];
 int kmain();
 int kcharlen(char *c);
 long khash(char* string);
-#endif
