6754770de43a37fd9d1292dcd45dea06a5925fea
Scripted <68810671+ScriptedDeveloper@users.noreply.github.com>
2022-03-08 21:50:54 +0100

    Fixing push mistakes

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -1,29 +1,22 @@
 #include "idt.h"
-#define DIVIDE_BY_ZERO 0
 
-
-__attribute__((noreturn))
-void exception_handler(){
-    __asm__ volatile("cli; hlt"); // Stops everything
+__attribute__((alligned(0x10)))
+struct idt_entry idt_en[256];
+struct idt_ptr idtp;
+void idt_set_desc(uint8_t num, uint64_t base, uint16_t segment, uint8_t flags){
+    idt_en[num].base_hi = (base >> 16) & 0xFFFF;
+    idt_en[num].base_lo = base & 0xFFFF;
+    idt_en[num].null = 0;
+    idt_en[num].segment = segment;
+    idt_en[num].flags = flags;
 }
 
-void idt_set_desc(uint8_t vector, void* isr, uint8_t flags){
-    IDTDesc* descriptor = &idt_desc[vector];
-    descriptor->isr_low        = (uint32_t)isr & 0xFFFF;
-    descriptor->kernel_cs      = 0x08;
-    descriptor->attributes     = flags;
-    descriptor->isr_high       = (uint32_t)isr >> 16;
-    descriptor->reserved       = 0;
-}
-    
-void idt_init(){
-    idtr_t_struct.base = (uintptr_t)&idt_desc;
-    idtr_t_struct.limit = (uint16_t)sizeof(IDTDesc) * INTERRUPTS_COUNT - 1;
-    for(uint8_t vector = 0; vector < 32; vector++){
-        idt_set_desc(vector, isr_stub_table[vector], 0x8E);
-        vector[&vector] = true;
-        __asm__ volatile ("lidt %0" : : "m"(idtr_t_struct)); // load the new IDT
-        __asm__ volatile ("sti"); // set the interrupt flag
-    }
 
+void idt_init() {
+    idtp.base = &idt_en;
+    idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
+    memset(&idt_en, 0, sizeof(struct idt_entry) * 256);
+    idt_loader();
+    isr_init();
+    print("IDT successfully loaded!\n");
 }
