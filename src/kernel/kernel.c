fc479affe9b494a9c243b079e4addbb1c5dab19e
Scripted <68810671+ScriptedDeveloper@users.noreply.github.com>
2022-03-12 19:29:31 +0100

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -19,13 +19,6 @@ void kpanic(char *msg) {
 
 kmain(multiboot_info_t* mbd, uint32_t magic){
 	terminal_clear_screen();
-	if(itoa(magic, 10) != itoa(MULTIBOOT_HEADER_MAGIC, 10)) { // Where the issue is
-		print("got   ");
-		print(itoa(magic, 10));
-		print(" but need ");
-		print(itoa(MULTIBOOT_BOOTLOADER_MAGIC, 10));
-			__asm__ volatile("cli; hlt");
-	}
 	gdt_init();
 	idt_init();
 	command_line();
